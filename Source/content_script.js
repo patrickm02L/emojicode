ae411124cfbe8ff0652138bd1f1115b76bf56f68
Patrick McDonnell <YOUR@EMAIL.compatrickm@codeforamerica.org>
2016-06-02 19:43:23 -0700

    tried to replace chicken with image

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,5 +1,32 @@
+var first_time_through_the_loop = true;
 
+function replaceTextOnPage(from, to){
+  getAllTextNodes().forEach(function(node){
+		debugger;
+		//node.nodeValue = node.nodeValue.replace(new RegExp(quote(from), 'g'), to);
+  });
+
+  function getAllTextNodes(){
+    var result = [];
+
+    (function scanSubTree(node){
+      if(node.childNodes.length)
+        for(var i = 0; i < node.childNodes.length; i++)
+          scanSubTree(node.childNodes[i]);
+      else if(node.nodeType == Node.TEXT_NODE)
+        result.push(node);
+    })(document);
+
+    return result;
+  }
+
+  function quote(str){
+    return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  }
+}
 
 $("body").bind("DOMSubtreeModified", function() {
-  console.log("Hello!");
+	window.setTimeout(function() {
+		replaceTextOnPage('chicken', '<img src="https://afeld.github.io/emoji-css/emoji/chicken.png"/>');
+	}, 0);
 });
