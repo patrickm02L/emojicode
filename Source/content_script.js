cd61ba8e03818d9109a0ff51ec0b5e7b9c55fcf5
Patrick McDonnell <YOUR@EMAIL.compatrickm@codeforamerica.org>
2016-06-09 19:20:59 -0700

    remove un-needed code

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,21 +1,14 @@
-var haystackText = "";
+
 function findMyText(needle, replacement) {
-     if (haystackText.length == 0) {
-          haystackText = document.getElementsByClassName("app-main")[0].innerHTML;
-     }
-     var match = new RegExp(needle, "ig");
-     var replaced = "";
-     if (replacement.length > 0) {
-          replaced = haystackText.replace(match, replacement);
-     }
-     else {
-          var boldText = "<div style=\"background-color: yellow; display: inline; font-weight: bold;\">" + needle + "</div>";
-          replaced = haystackText.replace(match, boldText);
-     }
-     document.getElementsByClassName("app-main")[0].innerHTML = replaced;
+  var haystackText = document.getElementsByClassName("app-main")[0].innerHTML;
+  var match = new RegExp(needle, "ig");
+  var replaced = "";
+  replaced = haystackText.replace(match, replacement);
+  document.getElementsByClassName("app-main")[0].innerHTML = replaced;
 }
 
 window.setTimeout(function() {
   console.log("running");
-	findMyText('chicken', '<img src="https://afeld.github.io/emoji-css/emoji/chicken.png"/>');
+  findMyText('chicken', '<img src="https://afeld.github.io/emoji-css/emoji/chicken.png"/>');
+  findMyText('goat', '<img src="https://afeld.github.io/emoji-css/emoji/goat.png"/>');
 }, 3000);
